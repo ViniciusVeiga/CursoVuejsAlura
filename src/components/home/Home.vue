69bde516bb54d4a78dee1c0b3d827d829a31e6c9
ricardo.antero321 <ricardo.antero321@hotmail.com>
2018-10-16 08:32:36 -0300

    Subindo

---
+++ src/components/home/Home.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <h1 class="titulo">{{ titulo }}</h1>
+
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre pelo título da foto">
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="item of filtrar" :key="item.id" >
+        <painel :titulo="item.titulo">
+          <imagem :url="item.url" :titulo="item.titulo"></imagem>
+        </painel>
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+import Painel from '../shared/painel/Painel.vue';
+import Imagem from '../shared/imagem/imagem.vue';
+
+export default {
+
+  components: {
+    'painel': Painel,
+    'imagem': Imagem
+  },
+
+  computed: {
+    filtrar() {
+
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.imagens.filter(imagem => exp.test(imagem.titulo))
+      } else {
+        return this.imagens;
+      }
+    }
+  },
+
+  data () {
+    return {
+      titulo: 'Alura',
+      imagens: [],
+      filtro: ''
+    }
+  },
+
+  created() {
+    
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(imagens => this.imagens = imagens, err => console.log(err));
+  }
+}
+</script>
+
+<style>
+
+  .titulo {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+</style>
---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <ul>
+            <li v-for="item in rotas" :key="item.id">
+                <router-link :to="item.path">{{item.titulo}}</router-link>
+            </li>
+        </ul>
+    </nav> 
+</template>
+
+<script>
+export default {
+    
+    props: {        
+        rotas: {
+            type: Array, 
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
